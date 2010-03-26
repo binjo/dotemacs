dc24be07dbdccbb355778d8341c3316ea9e0315c
binjo <binjo.cn@gmail.com>
2010-03-26 17:51:51 +0800

    Associate .hs with haskell-mode.

--- .emacs.d/config/binjo-program.el
+++ .emacs.d/config/binjo-program.el
@@ -147,3 +147,4 @@
       (shell-quote-argument "c:/Program Files/Haskell Platform/bin/ghci.exe"))
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-to-list 'auto-mode-alist '("\\.hs" . haskell-mode))
