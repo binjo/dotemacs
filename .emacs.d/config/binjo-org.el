07bcff655cb5a332cc734dfc0fcb08d3c56848f0
binjo <binjo.cn@gmail.com>
2010-10-14 15:42:06 +0800

    discard settings of org-remember.

--- .emacs.d/config/binjo-org.el
+++ .emacs.d/config/binjo-org.el
@@ -118,39 +118,7 @@
          "* %T %^{Description}\n %x"
          :empty-lines 1)))
 
-;;; TODO substitue org-remember with org-capture
-(org-remember-insinuate)
 (setq org-default-notes-file (concat org-directory "notes.org"))
-(global-set-key (kbd "C-c r") 'org-remember)
-(setq org-remember-templates
-  '(("Clipboard" ?c "* %T %^{Description}\n %x"
-     "remember.org" "Interesting")
-    ("ToDo" ?t "* TODO %T %^{Summary}"
-     "todo.org" "Todo")
-    ("Exploit" ?e "* %T %^{Description}\n %x"
-     "exploits.org" "Exploits")))
-
-;; http://metajack.im/2008/12/30/gtd-capture-with-emacs-orgmode/
-(defadvice remember-finalize (after delete-remember-frame activate)
-  "Advise remember-finalize to close the frame if it is the remember frame"
-  (if (equal "*Remember*" (frame-parameter nil 'name))
-    (delete-frame)))
-
-(defadvice remember-destroy (after delete-remember-frame activate)
-  "Advise remember-destroy to close the frame if it is the remember frame"
-  (if (equal "*Remember*" (frame-parameter nil 'name))
-      (delete-frame)))
-
-;; make the frame contain a single window. by default org-remember
-;; splits the window.
-(add-hook 'remember-mode-hook 'delete-other-windows)
-
-(defun make-remember-frame ()
-  "Create a new frame and run org-remember"
-  (interactive)
-  (make-frame '((name . "*Remember*") (width . 80) (height . 10)))
-  (select-frame-by-name "*Remember*")
-  (org-remember))
 
 ;; org-google-weather
 (setq org-google-weather-icon-directory
