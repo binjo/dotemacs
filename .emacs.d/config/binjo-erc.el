6ff07adfec76046bc722a67b8c0105a185f0d353
binjo <binjo.cn@gmail.com>
2011-06-23 15:20:14 +0800

    add new room for erc.

--- .emacs.d/config/binjo-erc.el
+++ .emacs.d/config/binjo-erc.el
@@ -70,11 +70,13 @@
 ;(setq erc-default-port "9940")
 (setq  erc-server-coding-system '(utf-8 . utf-8)
        erc-encoding-coding-alist '(("#segfault" . gb2312)
+                                   ("#music" . gb2312)
+                                   ("#superman" . gb2312)
                                    ("#ph4nt0m" . gb2312)
                                    ("&bitlbee" . gb2312)))
 (setq erc-autojoin-channels-alist
       '(("freenode.net" "#metasploit" "#corelan" "##re")
-        ("0x557.net" "#segfault" "#darpa")
+        ("0x557.net" "#segfault" "#darpa" "#music")
         ("oftc.net" "#emacs-cn")))
 
 ;; trim erc nicks
